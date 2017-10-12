44311f1b8bb3561b95e26a58b77de3741d54e5e2
Robbie <txt3rob@gmail.com>
2017-10-12 11:27:31 +0100

    updated

--- burp-browser/README.md
+++ burp-browser/README.md
@@ -1,4 +1,7 @@
 Burp Browser based on jgamblin/burpbrowser
 Added Burp free.
+VNC started on 7567
+
+docker run -d -p 7567:5901 txt3rob/burpbrowser
 
 Plans to auto-start burp with some defaults pre-set and add some addons.
--- burp-browser/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export DISPLAY=:1
-Xvfb :1 -screen 0 1600x900x16 &
-sleep 5
-openbox-session&
-x11vnc -display :1 -now -listen localhost -xkb -ncache 10 -ncache_cr -forever &
-ln -s /root/noVNC/vnc_auto.html /root/noVNC/index.html 
-./root/noVNC/utils/launch.sh --vnc localhost:5900
