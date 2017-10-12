9c2ed3287d9f4eda6303f7ccf07262dc77893783
Robbie <txt3rob@gmail.com>
2017-10-12 10:42:15 +0100

    Update startup.sh

--- burp-browser/startup.sh
+++ burp-browser/startup.sh
@@ -5,4 +5,4 @@ sleep 5
 openbox-session&
 x11vnc -display :1 -now -listen localhost -xkb -ncache 10 -ncache_cr -forever &
 ln -s /root/noVNC/vnc_auto.html /root/noVNC/index.html 
-./utils/launch.sh --vnc localhost:5900
+./root/noVNC/utils/launch.sh --vnc localhost:5900
