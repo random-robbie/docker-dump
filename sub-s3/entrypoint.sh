94f0090f78d4966af01d0621ddaff3de9eb6d60d
Robbie <txt3rob@gmail.com>
2017-11-27 15:14:52 +0000

    if AWS creds are not found
    updated entrypoint

--- sub-s3/entrypoint.sh
+++ sub-s3/entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-echo "[default]" >> ~/.aws/credentials
-echo "aws_access_key_id = $AWS_ACC" >> ~/.aws/credentials
-echo "aws_secret_access_key = $AWS_SEC" >> ~/.aws/credentials
+if [ ! -f ~/.aws/credentials ]; then
+    echo "[default]" >> ~/.aws/credentials
+	echo "aws_access_key_id = $AWS_ACC" >> ~/.aws/credentials
+	echo "aws_secret_access_key = $AWS_SEC" >> ~/.aws/credentials
+fi
+
+
+
 python /Sublist3r/sublist3r.py -d $DOMAIN -p 80,443 -v -o $DOMAIN.txt
 bash /Sublist3r/bucketeer.sh $DOMAIN.txt
 bash
