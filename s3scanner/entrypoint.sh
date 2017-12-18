f924e3d2624d92bbe1da1b1b87a946f4a7b920ee
Robbie <txt3rob@gmail.com>
2017-12-18 09:39:21 +0000

    Check after its completed.

--- s3scanner/entrypoint.sh
+++ s3scanner/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python /Sublist3r/sublist3r.py -b -d $DOMAIN -v -o /Sublist3r/$DOMAIN.txt
 python /S3Scanner/s3finder.py -g found.txt /Sublist3r/$DOMAIN.txt
+bash /S3Scanner/s3dumper.sh /found.txt
 bash
